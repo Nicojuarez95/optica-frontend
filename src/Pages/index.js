371ed889314b7e9c0a8e474c48a7c79ce8d8d08e
Nico <juareznico61@gmail.com>
2023-11-03 11:25:19 -0300

    proyecto

--- src/Pages/index.js
+++ src/Pages/index.js
@@ -2,7 +2,6 @@ import Login from "../../src/Pages/Login/Login.jsx"
 import Register from "../../src/Pages/Register/Register.jsx"
 import Index from "./Index/Index";
 import IndexLayout from "../Layouts/IndexLayout";
-
 import { createBrowserRouter } from "react-router-dom";
 
 export const router = createBrowserRouter([
